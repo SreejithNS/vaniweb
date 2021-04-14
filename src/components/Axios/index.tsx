9da80db936cc5ac9c0a7d3c6904106798c4b6ebc
Sreejith N Subramanian <nssreejith229@gmail.com>
2021-04-14 17:44:19 +0000

    baseUrl changed for mock server

--- src/components/Axios/index.tsx
+++ src/components/Axios/index.tsx
@@ -2,7 +2,8 @@ import { configure } from 'axios-hooks'
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: 'https://m10yyf9yzj.execute-api.us-east-1.amazonaws.com/'
+    //baseURL: 'https://m10yyf9yzj.execute-api.us-east-1.amazonaws.com/'
+    baseURL: 'https://c5040795-ff8d-4e5a-9831-65b87e682339.mock.pstmn.io/'
 })
 
 export const initAxios = () => configure({ axios });
